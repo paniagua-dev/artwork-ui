833a9a173fe0ae217df7f4ce80a3f8cb89b7f495
Julio Paniagua <julio.paniagua@factor8.ch>
2020-12-06 18:30:02 +0100

    Update config vue in order to handler the chunks file name

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const chunkPrefix = require('./package.json').name
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    extract: {
+      filename: `${chunkPrefix}.core.css`,
+      chunkFilename: `${chunkPrefix}.css`,
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: `${chunkPrefix}.core.js`,
+      chunkFilename: `${chunkPrefix}.js`,
+    }
+  },
 };
